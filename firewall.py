f950a3800bb2b935e8b8addd57ed4f1e35eb9b0f
Avery Pennarun <apenwarr@gmail.com>
2010-10-01 00:39:30 -0700

    BSD: sysctl net.inet.ip.forwarding=1 is not necessary.
    If your machine is a firewall/router, it affects whether people behind the
    router can use your sshuttle connection - in the same way that it affects
    whether they can route *anything* through you.  And thus, it should be set
    by the admin, not by sshuttle.
    
    sshuttle works fine for the local user either way.
    
    (This also affects MacOS since it's a BSD variant.)

--- firewall.py
+++ firewall.py
@@ -129,7 +129,6 @@ def do_ipfw(port, subnets):
 
     if subnets:
         sysctl_set('net.inet.ip.fw.enable', 1)
-        sysctl_set('net.inet.ip.forwarding', 1)
         sysctl_set('net.inet.ip.scopedroute', 0)
 
         ipfw('add', sport, 'check-state', 'ip',
