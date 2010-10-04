a32305a275eea70a2df22992eb0f4e7483d8ca98
Avery Pennarun <apenwarr@gmail.com>
2010-10-04 02:45:21 -0700

    server.py: don't send partial hostwatch lists.
    If hostwatch has a lot of stuff to say all at once, it would come in more
    than one recv() packet, and server.py would send each packet individually as
    a CMD_HOST_LIST message.  Unfortunately, client.py (rightly) expects each
    CMD_HOST_LIST message to be complete, ie. a correct sequence of rows.
    
    So now server.py makes sure of this.  If there's a leftover bit (ie. an
    unterminated line), it saves it for later.
    
    Bug reported by user "Duke" on the mailing list.

--- server.py
+++ server.py
@@ -133,12 +133,20 @@ def main():
     mux.send(0, ssnet.CMD_ROUTES, routepkt)
 
     hw = Hostwatch()
-
+    hw.leftover = ''
+        
     def hostwatch_ready():
         assert(hw.pid)
         content = hw.sock.recv(4096)
         if content:
-            mux.send(0, ssnet.CMD_HOST_LIST, content)
+            lines = (hw.leftover + content).split('\n')
+            if lines[-1]:
+                # no terminating newline: entry isn't complete yet!
+                hw.leftover = lines.pop()
+                lines.append('')
+            else:
+                hw.leftover = ''
+            mux.send(0, ssnet.CMD_HOST_LIST, '\n'.join(lines))
         else:
             raise Fatal('hostwatch process died')
 
