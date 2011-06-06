50849b86b0953b1ef1b7782e4756e99675419430
Brian May <brian@microcomaustralia.com.au>
2011-06-06 11:26:08 +1000

    This hack is IPv4 specific, ensure it doesn't get used for other address families.

--- ssnet.py
+++ ssnet.py
@@ -128,7 +128,8 @@ class SockWrapper:
             return  # already connected
         self.rsock.setblocking(False)
         debug3('%r: trying connect to %r\n' % (self, self.connect_to))
-        if socket.inet_aton(self.connect_to[0])[0] == '\0':
+        family = self.rsock.family
+        if family==socket.AF_INET and socket.inet_pton(family, self.connect_to[0])[0] == '\0':
             self.seterr(Exception("Can't connect to %r: "
                                   "IP address starts with zero\n"
                                   % (self.connect_to,)))
