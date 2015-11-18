eaad54f68b724753a9789369eb7219de66faf455
Brian May <brian@linuxpenguins.xyz>
2015-11-18 20:08:15 +1100

    Add FIXME comment.

--- sshuttle/methods/__init__.py
+++ sshuttle/methods/__init__.py
@@ -13,6 +13,7 @@ def original_dst(sock):
         sockaddr_in = sock.getsockopt(socket.SOL_IP,
                                       SO_ORIGINAL_DST, SOCKADDR_MIN)
         (proto, port, a, b, c, d) = struct.unpack('!HHBBBB', sockaddr_in[:8])
+        # FIXME: decoding is IPv4 only.
         assert(socket.htons(proto) == socket.AF_INET)
         ip = '%d.%d.%d.%d' % (a, b, c, d)
         return (ip, port)
