sshuttle/sshuttle

commit 1c46f25e1349e0f4cbacbd97d478fab73ac03b07

Fixed str being used as bytes in hostwatch
This should solve the TypeError reported in #53 and some others I found
while testing the fix.

Closes: #53

sshuttle/server.py
@@ -247,20 +247,20 @@ def main(latency_control):
     mux.send(0, ssnet.CMD_ROUTES, routepkt)
 
     hw = Hostwatch()
-    hw.leftover = ''
+    hw.leftover = b''
 
     def hostwatch_ready(sock):
         assert(hw.pid)
         content = hw.sock.recv(4096)
         if content:
-            lines = (hw.leftover + content).split('\n')
+            lines = (hw.leftover + content).split(b'\n')
             if lines[-1]:
                 # no terminating newline: entry isn't complete yet!
                 hw.leftover = lines.pop()
                 lines.append('')
             else:
-                hw.leftover = ''
-            mux.send(0, ssnet.CMD_HOST_LIST, '\n'.join(lines))
+                hw.leftover = b''
+            mux.send(0, ssnet.CMD_HOST_LIST, b'\n'.join(lines))
         else:
             raise Fatal('hostwatch process died')
 

sshuttle/hostwatch.py
@@ -36,7 +36,7 @@ def write_host_cache():
     try:
         f = open(tmpname, 'wb')
         for name, ip in sorted(hostnames.items()):
-            f.write('%s,%s\n' % (name, ip))
+            f.write(('%s,%s\n' % (name, ip)).encode("ASCII"))
         f.close()
         os.chmod(tmpname, 0o600)
         os.rename(tmpname, CACHEFILE)
@@ -122,7 +122,7 @@ def _check_netstat():
     argv = ['netstat', '-n']
     try:
         p = ssubprocess.Popen(argv, stdout=ssubprocess.PIPE, stderr=null)
-        content = p.stdout.read()
+        content = p.stdout.read().decode("ASCII")
         p.wait()
     except OSError as e:
         log('%r failed: %r\n' % (argv, e))

commit 4a954c547a5ec7b91d2d9122e284b33822caf634

fix byte/string bug introduced in 1c46f25e
This is the error message that this commit fixes:
TypeError: sequence item 142: expected a bytes-like object, str found

Complete what 1c46f25e started, more or less.

sshuttle/server.py
@@ -257,7 +257,7 @@ def main(latency_control):
             if lines[-1]:
                 # no terminating newline: entry isn't complete yet!
                 hw.leftover = lines.pop()
-                lines.append('')
+                lines.append(b'')
             else:
                 hw.leftover = b''
             mux.send(0, ssnet.CMD_HOST_LIST, b'\n'.join(lines))