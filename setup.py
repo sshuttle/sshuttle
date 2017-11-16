7a71ae1380152d1ddaa67980339bbf9e517c1c70
Brian May <brian@linuxpenguins.xyz>
2017-11-16 18:06:33 +1100

    Remove trailing whitespace

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 #
 # This file is part of sshuttle.
 #
-# sshuttle is free software: you can redistribute it and/or modify 
-# it under the terms of the GNU Lesser General Public License as 
-# published by the Free Software Foundation; either version 2.1 of 
+# sshuttle is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of
 # the License, or (at your option) any later version.
 #
 # sshuttle is distributed in the hope that it will be useful,
