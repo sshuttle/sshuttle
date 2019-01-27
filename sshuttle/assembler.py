8e35f049e2d8e5262f6e407a0e47f193f7480f20
Vasil Kolev <vasil@ludost.net>
2019-01-27 12:55:21 +0200

    auto-nets: retrieve routes only if using auto-nets
    There's a known issue that makes sshuttle crash if there are too
    many routes on the remote host (that don't fit in 64KB). This patch
    requests the routes only if auto-nets is specified on the command
    line.

--- sshuttle/assembler.py
+++ sshuttle/assembler.py
@@ -35,4 +35,5 @@ sshuttle.helpers.verbose = verbosity
 
 import sshuttle.cmdline_options as options
 from sshuttle.server import main
-main(options.latency_control, options.auto_hosts, options.to_nameserver)
+main(options.latency_control, options.auto_hosts, options.to_nameserver,
+     options.auto_nets)
