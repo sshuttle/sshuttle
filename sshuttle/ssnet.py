534ad8dfed8b803165b15157eea9d85e30924a59
Mark Heiges <mheiges@ibm.com>
2020-05-09 15:21:30 -0400

    fix crash triggered by port scans closing socket

--- sshuttle/ssnet.py
+++ sshuttle/ssnet.py
@@ -96,7 +96,10 @@ def _try_peername(sock):
             return '%s:%s' % (pn[0], pn[1])
     except socket.error:
         _, e = sys.exc_info()[:2]
-        if e.args[0] not in (errno.ENOTCONN, errno.ENOTSOCK):
+        if e.args[0] == errno.EINVAL:
+            debug2("_try_peername error: sock.getpeername() %s\nsocket is probably closed.\n" % e)
+            pass
+        elif e.args[0] not in (errno.ENOTCONN, errno.ENOTSOCK):
             raise
     except AttributeError:
         pass
