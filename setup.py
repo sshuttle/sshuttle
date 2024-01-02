cd2d69ac0825035ee499e17a45f89ca1a9b6257a
nom3ad <19239479+nom3ad@users.noreply.github.com>
2024-01-02 23:21:29 +0530

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sshuttle",
-    version='1.1.2',
+    version='1.2.0',
     url='https://github.com/sshuttle/sshuttle',
     author='Brian May',
     author_email='brian@linuxpenguins.xyz',
