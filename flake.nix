cda60a52331c7102cff892b9b77c8321e276680a
Brian May <brian@linuxpenguins.xyz>
2025-02-06 08:49:01 +1100

    fix: update nix flake to fix problems

--- flake.nix
+++ flake.nix
@@ -1,24 +1,32 @@
 {
-  description =
-    "Transparent proxy server that works as a poor man's VPN. Forwards over ssh. Doesn't require admin. Works with Linux and MacOS. Supports DNS tunneling.";
+  description = "Transparent proxy server that works as a poor man's VPN. Forwards over ssh. Doesn't require admin. Works with Linux and MacOS. Supports DNS tunneling.";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.poetry2nix = {
     url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      poetry2nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         p2n = import poetry2nix { inherit pkgs; };
-        overrides = p2n.defaultPoetryOverrides.extend (self: super: {
-          nh3 = super.nh3.override { preferWheel = true; };
-          bump2version = super.bump2version.overridePythonAttrs (old: {
-            buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
-          });
-        });
+        overrides = p2n.defaultPoetryOverrides.extend (
+          self: super: {
+            nh3 = super.nh3.override { preferWheel = true; };
+            bump2version = super.bump2version.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+            });
+          }
+        );
 
         poetry_env = p2n.mkPoetryEnv {
           python = pkgs.python3;
@@ -31,12 +39,18 @@
           inherit overrides;
         };
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         packages = {
           sshuttle = poetry_app;
           default = self.packages.${system}.sshuttle;
         };
-        devShells.default =
-          pkgs.mkShell { packages = [ pkgs.poetry poetry_env ]; };
-      });
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.poetry
+            poetry_env
+          ];
+        };
+      }
+    );
 }
